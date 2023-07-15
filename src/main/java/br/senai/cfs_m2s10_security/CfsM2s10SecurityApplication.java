f3152a391d1d053338a1d1f4cce01d7374576538
Eduardo Cesar Veras dos Santos <eduardocvsantos@gmail.com>
2023-07-15 15:28:11 -0300

    Commit inicial

---
+++ src/main/java/br/senai/cfs_m2s10_security/CfsM2s10SecurityApplication.java
@@ -0,0 +1,13 @@
+package br.senai.cfs_m2s10_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfsM2s10SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfsM2s10SecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/senai/cfs_m2s10_security/CfsM2s10SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senai.cfs_m2s10_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfsM2s10SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
